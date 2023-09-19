524e7382f2afd4b9be19637784ce85bd5616317a
Shand, Brian <Brian.Shand@fpl.com>
2023-09-19 09:27:24 -0400

    Initial Commit

--- lib/stock price app-stack.ts
+++ lib/stock price app-stack.ts
@@ -6,6 +6,9 @@ export class StockPriceAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // API KEY: 3U31896RG615ZMFY
+    // https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo
+
     // The code that defines your stack goes here
 
     // example resource
